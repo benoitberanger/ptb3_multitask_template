7c0d20165cf06b95320b57d13ae7fd5b883d72a5
Benoît Béranger <benoit.beranger@icm-institute.org>
2025-09-03 18:19:03 +0200

    FixationCross update
    What is the best ﬁxation target? The effect of target shape on stability of ﬁxational eye movements - L. Thaler , A.C. Schütz , M.A. Goodale, K.R. Gegenfurtner

--- +PTB_OBJECT/+VIDEO/@FixationCross/FixationCross.m
+++ +PTB_OBJECT/+VIDEO/@FixationCross/FixationCross.m
@@ -16,7 +16,12 @@ classdef FixationCross < PTB_OBJECT.VIDEO.Base
         width_px       (1,1) double
         center_x_px    (1,1) double
         center_y_px    (1,1) double
-        allCoords      (4,2) double % pre-calculated coordinates of the cross for PTB, in pixels
+        coord_rects    (4,2) double % pre-calculated coordinates of the cross for PTB, in pixels
+        coord_dot      (1,4) double
+        coord_mask     (1,4) double
+        width_mask     (1,1) double
+        coord_frame    (1,4) double
+        color_frame    (1,4) uint8  % [R G B a] from 0 to 255
     end % props
 
     methods(Access = public)
@@ -37,15 +42,30 @@ classdef FixationCross < PTB_OBJECT.VIDEO.Base
             % re-compute
             hRect = [0 0 self.dim_px   self.width_px ];
             vRect = [0 0 self.width_px self.dim_px   ];
-            self.allCoords = [
+            self.coord_rects = [
                 CenterRectOnPoint(hRect, self.center_x_px, self.center_y_px)
                 CenterRectOnPoint(vRect, self.center_x_px, self.center_y_px)
                 ]';
+
+            dotRect = [0 0 self.width_px self.width_px]*0.8;
+            self.coord_dot = CenterRectOnPoint(dotRect, self.center_x_px, self.center_y_px);
+
+            h = sqrt(self.dim_px^2 + self.width_px^2);
+            self.width_mask = h - self.dim_px;
+            maskRect = [0 0 h+1 h+1];
+            self.coord_mask = CenterRectOnPoint(maskRect, self.center_x_px, self.center_y_px);
+
+            frameRect = [0 0 self.dim_px+2 self.dim_px+2];
+            self.coord_frame = CenterRectOnPoint(frameRect, self.center_x_px, self.center_y_px);
+            self.color_frame = [self.color(1)/2 self.color(2)/2 self.color(3)/2 self.color(4)];
         end % fcn
 
         %------------------------------------------------------------------
         function Draw( self )
-            Screen('FillRect', self.window.ptr, self.color, self.allCoords);
+            Screen('FillRect' , self.window.ptr, self.color          , self.coord_rects);
+            Screen('FillOval' , self.window.ptr, self.window.bg_color, self.coord_dot  );
+            Screen('FrameOval', self.window.ptr, self.window.bg_color, self.coord_mask , self.width_mask/2);
+            Screen('FrameOval', self.window.ptr, self.color_frame    , self.coord_frame, 1);
         end % fcn
 
     end % meths
--- +TASK/cfgFixationCross.m
+++ +TASK/cfgFixationCross.m
@@ -1,7 +1,7 @@
 function cfg = cfgFixationCross()
 
-cfg.Size     = 0.10;              %  Size_px = ScreenY_px * Size
-cfg.Width    = 0.10;              % Width_px =    Size_px * Width
+cfg.Size     = 0.03;              %  Size_px = ScreenY_px * Size
+cfg.Width    = 0.30;              % Width_px =    Size_px * Width
 cfg.Color    = [127 127 127 255]; % [R G B a], from 0 to 255
 cfg.Position = [0.50 0.50];       % Position_px = [ScreenX_px ScreenY_px] .* Position
 
